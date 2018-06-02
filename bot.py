bd1d60628529f66b759215dd89614dc3b289c420
Magne Davidsen <magne.davidsen@gmail.com>
2018-06-02 09:45:36 +0200

    Only care about files in #pizza channel, and only care about RSVPs in DMs
    Should have been this way in the first place, but necessary now that we add the bot to more channels 😮

--- bot.py
+++ bot.py
@@ -9,36 +9,50 @@ import base64
 from slackclient import SlackClient
 from time import sleep
 
+pizza_channel_id = 'C2NC8DBN1'
+
 slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
+
+
+def is_dm(message):
+    return message['channel'][0] == 'D'
+
 
 if sc.rtm_connect():
     while True:
         event_list = sc.rtm_read()
-        message_list = list(filter(lambda m: m['type'] == 'message', event_list))
+        message_list = list(
+            filter(lambda m: m['type'] == 'message', event_list))
         for message in message_list:
-            if 'file' in message:
-                api.send_slack_message(message['channel'], u'Takk for fil! 🤙')
-                headers = {u'Authorization': u'Bearer %s' % slack_token}
-                r = requests.get(message['file']['url_private'], headers=headers)
-                b64 = base64.b64encode(r.content)
-                payload = {'file': 'data:image;base64,%s' % b64, 'upload_preset': 'blank.pizza'}
-                r2 = requests.post('https://api.cloudinary.com/v1_1/blank/image/upload', data=payload)
-                api.save_image(r2.json()['public_id'], message['file']['user'], message['file']['title'])
-            else:
-                print message['user']
-                print api.get_invited_users()
+            if(message['channel'] == pizza_channel_id):
+                if 'file' in message:
+                    api.send_slack_message(
+                        message['channel'], u'Takk for fil! 🤙')
+                    headers = {u'Authorization': u'Bearer %s' % slack_token}
+                    r = requests.get(
+                        message['file']['url_private'], headers=headers)
+                    b64 = base64.b64encode(r.content)
+                    payload = {'file': 'data:image;base64,%s' % b64,
+                               'upload_preset': 'blank.pizza'}
+                    r2 = requests.post(
+                        'https://api.cloudinary.com/v1_1/blank/image/upload', data=payload)
+                    api.save_image(
+                        r2.json()['public_id'], message['file']['user'], message['file']['title'])
+            elif(is_dm(message)):
                 if message['user'] in api.get_invited_users():
                     if message['text'].lower() == 'ja':
                         api.rsvp(message['user'], 'attending')
-                        api.send_slack_message(message['channel'], u'Sweet! 🤙')
+                        api.send_slack_message(
+                            message['channel'], u'Sweet! 🤙')
                         api.finalize_event_if_complete()
                     elif message['text'].lower() == 'nei':
                         api.rsvp(message['user'], 'not attending')
                         api.send_slack_message(message['channel'], u'Ok 😏')
                         api.invite_if_needed()
                     else:
-                        api.send_slack_message(message['channel'], u'Hehe jeg er litt dum, jeg. Skjønner jeg ikke helt hva du mener 😳. Kan du være med? (ja/nei)')
+                        api.send_slack_message(
+                            message['channel'], u'Hehe jeg er litt dum, jeg. Skjønner jeg ikke helt hva du mener 😳. Kan du være med? (ja/nei)')
         sleep(0.5)
 
 else:
