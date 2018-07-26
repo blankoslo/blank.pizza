6c192e38f10d6faf0ad1dec3aab92262cfbfdbcc
Magne Davidsen <magne.davidsen@gmail.com>
2018-07-26 09:50:00 +0200

    Try to parse request from Slack action, according to example

--- web_api.py
+++ web_api.py
@@ -1,5 +1,19 @@
 from flask import Flask, request
+from urllib.parse import unquote_plus
+import json
+import re
 app = Flask(__name__)
+
+
+def parse_request(request):
+    """
+    Parse the Slack POST request.
+    """
+    payload = request.get_data()
+    payload = unquote_plus(payload)
+    payload = re.sub('payload=', '', payload)
+    payload = json.loads(payload)
+    return payload
 
 
 @app.route("/")
@@ -9,7 +23,6 @@ def hello():
 
 @app.route("/api/action", methods=['GET', 'POST'])
 def action():
-    print(request.is_json)
-    content = request.get_json()
-    print(content)
-    return 'JSON posted'
+    payload = parse_request(request)
+    print(payload)
+    return 'Jabba'
