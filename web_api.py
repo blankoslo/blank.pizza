e2e5bc0c5ea222ed3b163345c5c8820704226bf0
Magne Davidsen <magne.davidsen@gmail.com>
2018-07-30 14:20:50 +0200

    Inspect the payload from slack by printing it

--- web_api.py
+++ web_api.py
@@ -10,8 +10,11 @@ def hello():
 
 @app.route("/api/action", methods=['GET', 'POST'])
 def action():
-    requestDict = json.loads(request.data.decode("utf-8"))
-    print(requestDict)
-    team_id = requestDict['team']['id']
-    user_id = requestDict['user']['id']
+    # requestDict = json.loads(request.data.decode("utf-8"))
+    print(type(request.data))
+    print(request.data)
+
+    # print(requestDict)
+    # team_id = requestDict['team']['id']
+    # user_id = requestDict['user']['id']
     return 'Jabba'
