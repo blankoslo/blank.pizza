d7e943ae8baf716d22c87b3675df34a593aacd84
Magne Davidsen <magne.davidsen@gmail.com>
2018-09-02 10:04:48 +0200

    Use response_url for async reponse to button clicks
    Responding to post request from slack times out randomly

--- web_api.py
+++ web_api.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, Response
+import requests
 import json
 import api
 app = Flask(__name__)
@@ -17,29 +18,35 @@ def action():
     payload = json.loads(request.form["payload"])
     # team_id = requestDict['team']['id']
     responses = []
+    response_url = payload['response_url']
 
     for action in payload['actions']:
         responses.append(button_rsvp(
-            payload['user']['id'], action['value'], payload['original_message']))
+            payload['user']['id'], action['value'], payload['original_message'], response_url))
 
     return Response(response=responses[0], mimetype='application/json')
 
 
-def button_rsvp(user_id, rsvp, original_message):
+def button_rsvp(user_id, rsvp, original_message, response_url):
     if user_id in api.get_invited_users():
         api.rsvp(user_id, rsvp)
         if(rsvp == "attending"):
             api.finalize_event_if_complete()
-            return response_message(original_message, "✅ Sweet! Det blir sykt nice 😋")
+            response_JSON = response_message(
+                original_message, "✅ Sweet! Det blir sykt nice! 😋")
+            requests.post(response_url, json=response_JSON)
         elif (rsvp == "not attending"):
             api.invite_if_needed()
-            return response_message(original_message, "⛔️ Ah, ok. Neste gang! 🤝")
+            response_JSON = response_message(
+                original_message, "⛔️ Ah, ok. Neste gang! 🤝")
+            requests.post(response_url, json=response_JSON)
     else:
-        return response_message(original_message, "💣 Hmm, hva har du gjort for noe rart nå?")
+        response_JSON = response_message(
+            original_message, "💣 Hmm, hva har du gjort for noe rart nå?")
+        requests.post(response_url, json=response_JSON)
 
 
 def response_message(original_message, text):
     original_message['attachments'] = [{'text': text}]
-    original_message['replace_original'] = True
 
     return json.dumps(original_message)
