a083edb284306d27eb209736837b9c311d3c2b54
Magne Davidsen <magne.davidsen@gmail.com>
2020-02-04 09:48:55 +0100

    Congratulate and welcome in thread (in an attempt to encourage thread congratulations/welcomes)

--- goodmorning.py
+++ goodmorning.py
@@ -21,22 +21,21 @@ def create_mention_string(slack_ids_map):
         return mention_string + " og %s" % slack_ids[-1]
 
 
-def mention_people(people, message):
+def mention_people(people, message, thread):
     slack_ids = db.get_slack_ids_from_emails(people)
 
     if len(slack_ids) > 0:
         mention_ids = map(lambda x: '<@%s>' % x, slack_ids)
         mention_string = create_mention_string(mention_ids)
-        return api.send_slack_message(
+        resp = api.send_slack_message(
             channel, message % mention_string)
+        api.send_slack_message(channel, thread, thread_ts=resp['ts'])
 
 
 api.sync_db_with_slack_and_return_count()
 
-first_day_resp = mention_people(floq_db.get_users_with_first_day(),
-                                "I dag har %s sin første dag i Blank!")
-api.send_slack_message(channel, "Velkommen 🌹", thread_ts=first_day_resp['ts'])
+mention_people(floq_db.get_users_with_first_day(),
+               "I dag har %s sin første dag i Blank!", "Velkommen 🌹")
 
-birthday_resp = mention_people(floq_db.get_users_with_birthday(),
-                               "I dag har %s bursdag!")
-api.send_slack_message(channel, "Gratulerer 🎈", thread_ts=birthday_resp['ts'])
+mention_people(floq_db.get_users_with_birthday(),
+               "I dag har %s bursdag!", "Gratulerer 🎈")
