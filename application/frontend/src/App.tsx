a703984e05e24b62177c61a4cc973a378848c76a
AndreasJJ <andreasjj@gmail.com>
2023-02-04 15:49:56 +0100

    update deps and http to https redirect client side

--- application/frontend/src/App.tsx
+++ application/frontend/src/App.tsx
@@ -8,32 +8,35 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import theme from './theme';
+import HttpsRedirect from 'react-https-redirect';
 
 const queryClient = new QueryClient();
 
 export const App: React.FC = () => {
     return (
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <ThemeProvider theme={theme}>
-                <QueryClientProvider client={queryClient}>
-                    <StoreProvider initState={initialState}>
-                        <Router />
-                        <ToastContainer
-                            position="top-center"
-                            autoClose={5000}
-                            hideProgressBar={false}
-                            newestOnTop
-                            closeOnClick
-                            rtl={false}
-                            pauseOnFocusLoss
-                            draggable
-                            pauseOnHover
-                        />
-                        <CssBaseline />
-                    </StoreProvider>
-                </QueryClientProvider>
-            </ThemeProvider>
-        </LocalizationProvider>
+        <HttpsRedirect>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <ThemeProvider theme={theme}>
+                    <QueryClientProvider client={queryClient}>
+                        <StoreProvider initState={initialState}>
+                            <Router />
+                            <ToastContainer
+                                position="top-center"
+                                autoClose={5000}
+                                hideProgressBar={false}
+                                newestOnTop
+                                closeOnClick
+                                rtl={false}
+                                pauseOnFocusLoss
+                                draggable
+                                pauseOnHover
+                            />
+                            <CssBaseline />
+                        </StoreProvider>
+                    </QueryClientProvider>
+                </ThemeProvider>
+            </LocalizationProvider>
+        </HttpsRedirect>
     );
 };
 
--- application/frontend/src/decs.d.ts
+++ application/frontend/src/decs.d.ts
@@ -6,3 +6,5 @@ declare module '*.svg' {
     const content: any;
     export default content;
 }
+
+declare module 'react-https-redirect';
