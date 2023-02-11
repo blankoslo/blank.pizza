539f43e82b36f627d086708a8f9f7647210ccdca
Andreas Jensen Jonassen <andreasjj@Andreass-MacBook-Pro.local>
2023-02-12 00:07:56 +0100

    refactor into function

--- application/bot/main.py
+++ application/bot/main.py
@@ -19,27 +19,16 @@ from src.broker.handlers import on_message
 
 pizza_channel_id = os.environ["PIZZA_CHANNEL_ID"]
 
-def main():
-    # Set up injector
-    api_config = BotApiConfiguration(pizza_channel_id, pytz.timezone('Europe/Oslo'))
-    injector.binder.bind(BotApiConfiguration, to=api_config)
-
-    # Set up logging
+def setup_logger():
     logger = logging.getLogger(__name__)
     logging_handler = logging.StreamHandler(sys.stdout)
     logging_handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s"))
     logger.addHandler(logging_handler)
     logger.setLevel(logging.DEBUG)
     logger.propagate = False
-    injector.binder.bind(logging.Logger, to=logger, scope=singleton)
+    return logger
 
-    # Try setting locale
-    try:
-        locale.setlocale(locale.LC_ALL, "nb_NO.utf8")
-    except:
-        logger.warning("Missing locale nb_NO.utf8 on server")
-
-    # Set up rabbitmq
+def setup_consumption_queue_listener():
     mq = AmqpConnection()
     mq.connect()
     mq.setup_exchange()
@@ -49,6 +38,24 @@ def main():
         mq.consume(on_message)
     consuming_thread = threading.Thread(target = consume)
     consuming_thread.start()
+
+def main():
+    # Set up injector
+    api_config = BotApiConfiguration(pizza_channel_id, pytz.timezone('Europe/Oslo'))
+    injector.binder.bind(BotApiConfiguration, to=api_config)
+
+    # Set up logging
+    logger = setup_logger()
+    injector.binder.bind(logging.Logger, to=logger, scope=singleton)
+
+    # Try setting locale
+    try:
+        locale.setlocale(locale.LC_ALL, "nb_NO.utf8")
+    except:
+        logger.warning("Missing locale nb_NO.utf8 on server")
+
+    # Set up rabbitmq
+    setup_consumption_queue_listener()
 
     # start scheduler
     scheduler.start()
