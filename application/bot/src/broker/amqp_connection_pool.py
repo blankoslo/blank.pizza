cf564a7e990863b021eb560e7d57086e7d451973
Andreas Jensen Jonassen <andreasjj@Andreass-MacBook-Pro.local>
2023-02-13 13:12:14 +0100

    changed to condition instead of lock

--- application/bot/src/broker/amqp_connection_pool.py
+++ application/bot/src/broker/amqp_connection_pool.py
@@ -18,23 +18,24 @@ class AmqpConnectionPool:
         self._connections_count = 0
         # Queue that keep tracks of all connections not in use
         self._wait_queue = queue.Queue(max_connections)
-        self._lock = threading.Lock()
+        self._condition = threading.Condition()
         self._timeout = timeout
 
     def get_connection(self):
-        with self._lock:
+        with self._condition:
             return self._get_connection()
 
     def release_connection(self, connection):
         # Put connection into wait_queue to indicate it's ready to be used
-        with self._lock:
+        with self._condition:
             self._wait_queue.put(connection)
             self._connections_count -= 1
+            self._condition.notify()
 
     def _get_connection(self):
         if not self._wait_queue.empty():
-            con = self._try_get_connection()
-            return con
+            connection = self._try_get_connection()
+            return connection
 
         if self._connections_count < self._max_connections:
             connection = self._create_and_get_connection()
@@ -46,17 +47,19 @@ class AmqpConnectionPool:
     def _try_get_connection(self):
         try:
             connection = self._get_connection_from_queue()
-
-            if connection.is_closed or connection._impl.is_closing:
-                return self._get_connection()
-
-            return connection
         except queue.Empty:
-            raise Exception("Timeout reached while waiting for a connection to become available")
+            if not self._condition.wait(self._timeout):
+                raise Exception("Timeout reached while waiting for a connection to become available")
+            connection = self._get_connection_from_queue()
+
+        if connection.is_closed or connection._impl.is_closing:
+            return self._get_connection()
+
+        return connection
 
     def _get_connection_from_queue(self):
         # Try to get a connection ready to be used from wait queue
-        connection = self._wait_queue.get(timeout=self._timeout)
+        connection = self._wait_queue.get()
         self._wait_queue.task_done()
         # Added connection to connections queue to indicate it's in use
         self._connections_count += 1
