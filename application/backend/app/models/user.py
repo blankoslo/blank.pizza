622e6c0591ab6035fd6997f49a6224cdd243da85
Andreasjj <andreasjj@gmail.com>
2023-03-05 20:00:51 +0100

    fixed bug, unique email constraint

--- application/backend/app/models/user.py
+++ application/backend/app/models/user.py
@@ -7,7 +7,7 @@ from app.models.mixins import CrudMixin
 class User(CrudMixin, db.Model):
     __tablename__ = "users"
     id = sa.Column(db.String, primary_key=True)
-    email = sa.Column(db.String, nullable=False, unique=True)
+    email = sa.Column(db.String, nullable=False)
     name = sa.Column(db.String, nullable=False)
     picture = sa.Column(db.String, nullable=False)
     slack_organization_id = sa.Column(sa.String, sa.ForeignKey('slack_organizations.team_id'), nullable=True)
